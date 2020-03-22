935c10d16f2404078b659203c537dc1c02c8235a
flaviostutz <flaviostutz@gmail.com>
2020-03-22 15:41:35 -0300

    first working version

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20161002113705-648efa622239 // indirect
 	github.com/gliderlabs/ssh v0.3.0
+	github.com/kr/pty v1.1.8
 	github.com/lestrrat-go/jwx v0.9.1
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.4.2
--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
+	"os"
+	"os/exec"
 	"strings"
+	"syscall"
+	"unsafe"
 
 	"github.com/gliderlabs/ssh"
+	"github.com/kr/pty"
 	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwt"
 	"github.com/sirupsen/logrus"
@@ -15,7 +19,9 @@ import (
 
 func main() {
 
-	log.Println("Starting ssh server on port 2222...")
+	logrus.SetLevel(logrus.DebugLevel)
+
+	logrus.Infof("Starting ssh server on port 2222...")
 
 	forwardHandler := &ssh.ForwardedTCPHandler{}
 
@@ -24,33 +30,41 @@ func main() {
 			claim0 := ctx.Value("lfw")
 			claim := claim0.(string)
 			if claim == "" {
-				logrus.Infof("No local forward claims found in JWT")
+				logrus.Infof("No local forward claims found in token")
 				return false
 			}
 
-			cps := strings.Split(claim, " ")
-			accept := false
-			for _, cp := range cps {
-				c1cs := strings.Split(cp, ":")
-				if c1cs[0] == fmt.Sprintf("%s:%d", dhost, dport) {
-					accept = true
-					break
-				}
-			}
+			accept := matchClaim(claim, dhost, dport)
+
 			if !accept {
-				logrus.Debugf("Forward %s:%s is NOT authorized", dhost, dport)
+				logrus.Infof("Forward %s:%d is NOT authorized (direct-tcpip)", dhost, dport)
 				return false
 			}
-			logrus.Debugf("Forward %s:%s authorized", dhost, dport)
+			logrus.Debugf("Forward %s:%d is authorized (direct-tcpip)", dhost, dport)
 
 			return true
 		}),
-		ReversePortForwardingCallback: ssh.ReversePortForwardingCallback(func(ctx ssh.Context, host string, port uint32) bool {
-			logrus.Debugf("Attempt to bind %s:%s", host, port)
+		ReversePortForwardingCallback: ssh.ReversePortForwardingCallback(func(ctx ssh.Context, bindHost string, port uint32) bool {
+			claim0 := ctx.Value("lfw")
+			claim := claim0.(string)
+			if claim == "" {
+				logrus.Infof("No remote forward claims found in token")
+				return false
+			}
+
+			accept := matchClaim(claim, bindHost, port)
+
+			if !accept {
+				logrus.Infof("Remote bind %s:%d is NOT authorized (tcpip-forward)", bindHost, port)
+				return false
+			}
+			logrus.Debugf("Remote bind %s:%d is authorized (tcpip-forward)", bindHost, port)
+
 			return true
 		}),
 		PasswordHandler: ssh.PasswordHandler(func(ctx ssh.Context, password string) bool {
-			//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiIyMzQyNDM0NTM0NTMiLCJtaWQiOiJHVEUzNDU2IiwiZXhwIjoxNTg3NTI5NjkzLCJyZnciOiIwLjAuMC4wOjQzNDMiLCJsZnciOiIyMDEuMjEuNDMuNDU6ODA4MCJ9.iaUGlrO-3HWdE-8irizqMfHLYV0Ctiu3N3qdEdirwJk
+			//SAMPLE: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiIyMzQyNDM0NTM0NTMiLCJtaWQiOiJHVEUzNDU2IiwiZXhwIjoxNTg3NTI5NjkzLCJyZnciOiIwLjAuMC4wOjQzNDMiLCJsZnciOiIyMDEuMjEuNDMuNDU6ODA4MCJ9.iaUGlrO-3HWdE-8irizqMfHLYV0Ctiu3N3qdEdirwJk
+			//SAMPLE2: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiIyMzQyNDM0NTM0NTMiLCJtaWQiOiJHVEUzNDU2IiwiZXhwIjoxNTg3NTI5NjkzLCJyZnciOiIwLjAuMC4wOjQzNDMiLCJsZnciOiIxMC4xLjEuMjU0OjgwIn0.ynmGKtRJyr5KowmD34m3A4OBnMdcmj9GCC0Vt3oyZHc
 			tokenString := password
 			token, err := jwt.Parse(bytes.NewReader([]byte(tokenString)), jwt.WithVerify(jwa.HS256, []byte("123")))
 			if err != nil {
@@ -92,10 +106,39 @@ func main() {
 			return true
 		}),
 		Addr: ":2222",
-		Handler: ssh.Handler(func(s ssh.Session) {
-			io.WriteString(s, "Tunnels prepared. Waiting for connections...\n")
-			select {}
+		PtyCallback: ssh.PtyCallback(func(ctx ssh.Context, pty ssh.Pty) bool {
+			return true
 		}),
+		Handler: ssh.Handler(func(s ssh.Session) {
+			io.WriteString(s, "Waiting for connections...\n")
+
+			cmd := exec.Command("sh")
+			ptyReq, winCh, isPty := s.Pty()
+			if isPty {
+				cmd.Env = append(cmd.Env, fmt.Sprintf("TERM=%s", ptyReq.Term))
+				f, err := pty.Start(cmd)
+				if err != nil {
+					panic(err)
+				}
+				go func() {
+					for win := range winCh {
+						setWinsize(f, win.Width, win.Height)
+					}
+				}()
+				go func() {
+					io.Copy(f, s) // stdin
+				}()
+				io.Copy(s, f) // stdout
+				cmd.Wait()
+			} else {
+				io.WriteString(s, "No PTY requested.\n")
+				s.Exit(1)
+			}
+		}),
+		ChannelHandlers: map[string]ssh.ChannelHandler{
+			"direct-tcpip": ssh.DirectTCPIPHandler,
+			"session":      ssh.DefaultSessionHandler,
+		},
 		RequestHandlers: map[string]ssh.RequestHandler{
 			"tcpip-forward":        forwardHandler.HandleSSHRequest,
 			"cancel-tcpip-forward": forwardHandler.HandleSSHRequest,
@@ -104,5 +147,25 @@ func main() {
 		// MaxTimeout:  120 * time.Second,
 	}
 
-	log.Fatal(server.ListenAndServe())
+	err := server.ListenAndServe()
+	if err != nil {
+		logrus.Errorf("Startup error %s", err)
+	}
+}
+
+func matchClaim(claim string, host string, port uint32) bool {
+	cps := strings.Split(claim, " ")
+	accept := false
+	for _, cp := range cps {
+		if cp == fmt.Sprintf("%s:%d", host, port) {
+			accept = true
+			break
+		}
+	}
+	return accept
+}
+
+func setWinsize(f *os.File, w, h int) {
+	syscall.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(syscall.TIOCSWINSZ),
+		uintptr(unsafe.Pointer(&struct{ h, w, x, y uint16 }{uint16(h), uint16(w), 0, 0})))
 }
